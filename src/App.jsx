040eeb1286f2fbf6207a64a833c56e461c01baff
AllenCodess <Aph296@protonmail.com>
2025-10-07 23:04:13 -0400

    imported styling from app.jsx instead of having that code in my app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import {format} from 'date-fns' //importing format from date-fns
+import './App.css' // importing styling from App.css
 
-export default function CurrentTime() { //function that styles the text content and displays current time/date
+export default function CurrentTime() { //function that displays current time/date
   return (
-    <body style={{                      // styling of the page
-      textAlign: "center",
-      fontSize: 50,
-      width:700,
-      margin: "0 auto",
-     fontFamily: "Orbitron",
-     color: "red", 
-      backgroundColor: "lightgray",
-
-    }}> 
+    <body>                      
       <p>{format ( new Date(), 'hh:mm EEEE, MMMM do Y')}</p>
       </body>
   )
 }
 
-// current time in the above syntax
